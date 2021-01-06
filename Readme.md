68e2b0721b774b12467762549e1f3e1de4c740a0
Knut <kwaale@github.com>
2021-01-06 14:41:56 -0300

    Styles

---
+++ Readme.md
@@ -0,0 +1,10 @@
+**Tres en linea**
+Tres en linea, se hizo como parte de un ejercicio práctico después de haber realizado curso de HTML, CSS y JavaScript con la gente de punchit *[Punchit][1]*. Con la intención de colgarlo en mi *[portafolio web][2]* y que a la vez los que quieran puedan disfrutar del juego, teniendo una experiencia divertida. Por otra parte con el transcurso de mi experiencia y cualquier sugerencia de la comunidad, ir mejorando la experiencia del usuario.
+Dejo un agradecimiento al profesor Franco Di Leo por compartir su experiencia con otras personas.
+Para poder usarlo es suficiente con entrar en *[Tres en linea][3]*.
+Autor: Knut Waale *[repositorio git][4]*.
+
+[1]:shop.punchit.com.ar
+[2]:https://kwaale.github.io/
+[3]:https://kwaale.github.io/TaTeTi/
+[4]:https://github.com/kwaale
--- docs/script.js
+++ docs/script.js
@@ -1,40 +1,68 @@
+//Favicon
+// - La foto esta demasiado pegada al borde izquierdo, dale espacio
+// - ponerle Favicon a la página y subpaginas. No se si tu foto sea un
+// buen Favicon para el cv 
+// - no me gusta mucho el fondo, no se ve tan serio. Se que tu no eres diseñador
+// y que no vendes eso, pero creo que una página con una imagen más moderna
+// y limpia atrae más
+//*************Corregir que aparezca nombre ganador */
+let turno;
+let arr;
 
-let turno = "";
 function start(){
+    turno = "";
+    arr = [0,1,2,3,4,5,6,7,8];
+    document.getElementById('jugadorX').addEventListener('submit', ()=>console.log("LISTENER"))
+    document.getElementById('jugadorO').addEventListener('blur', ()=>console.log("LISTENER Blur"))
     for(let i = 0; i < 9; i++){
         document.getElementById(`bt${i}`).addEventListener("click", cambia)
     }
-    
 }
+
 //Solo para iterar entre "X" y "O" y controlar que no se repita.
 function cambia(e){
-    // console.log("Entra en cambio",e.target.id)
     const btnId = e.target.id;
-    const content = document.getElementById(btnId);   
-    //console.log("Entra en cambio",content.value)
+    const content = document.getElementById(btnId);
+    if (!content.value)console.log("content.value", content.value);
     if (content.value !== "X" && content.value !== "O") {
         turno === "X" ? turno ="O":turno = "X";
-        content.value = turno
-        ganardor();    
+        content.value = turno;
+        ganardor(btnId);
+    }
+    
+}
+//Creando elemento ganador
+const elementoWin = (a)=>{
+    const jugadorX = document.getElementById('jugadorX').value;
+    const jugadorO = document.getElementById('jugadorO').value;
+    //const ganador = document.createElement('h1')
+    //Devuelve quien gana si esta vacio el input
+    if(jugadorO === "" || jugadorX === ""){
+        document.getElementById('ganador').innerHTML = `Gano la ${a}`;
+
+    //Devuelve quien gana con el valor del input
+    }else{
+        a == 'X' ?
+        document.getElementById('ganador').innerHTML = `Gano ${jugadorX}`:
+        document.getElementById('ganador').innerHTML = `Gano ${jugadorO}`;
     }
 }
-const ganardor = ()=>{
-    const arr = [];
-    for(let i = 0; i < 9; i++){
-        arr.push(document.getElementById(`bt${i}`).value);
-    }
-    console.log(arr, arr[3],arr[4],arr[5])
+//Evalua cuando hay linea de tres iguales
+const ganardor = (id)=>{
+        const num = document.getElementById(id).id.slice(2);//bt1
+        arr[num]=(document.getElementById(id).value);
+        console.log(arr);
     //Horizontales
-    if(arr[0] === arr[1] && arr[1] === arr[2])return console.log("Ganador",arr[0])
-    if(arr[3] === arr[4] && arr[4] === arr[5])return console.log("Ganador",arr[3])
-    if(arr[6] === arr[7] && arr[7] === arr[8])return console.log("Ganador",arr[6])
+    if(arr[0] === arr[1] && arr[1] === arr[2])return elementoWin(arr[0]);
+    if(arr[3] === arr[4] && arr[4] === arr[5])return elementoWin(arr[3]);
+    if(arr[6] === arr[7] && arr[7] === arr[8])return elementoWin(arr[6]);
     //Verticales
-    if(arr[0] === arr[3] && arr[3] === arr[6])return console.log("Ganador",arr[0])
-    if(arr[1] === arr[4] && arr[4] === arr[7])return console.log("Ganador",arr[1])
-    if(arr[2] === arr[5] && arr[5] === arr[8])return console.log("Ganador",arr[2])
+    if(arr[0] === arr[3] && arr[3] === arr[6])return elementoWin(arr[0]);
+    if(arr[1] === arr[4] && arr[4] === arr[7])return elementoWin(arr[1]);
+    if(arr[2] === arr[5] && arr[5] === arr[8])return elementoWin(arr[2]);
     //Diagonales
-    if(arr[0] === arr[4] && arr[4] === arr[8])return console.log("Ganador",arr[0])
-    if(arr[2] === arr[4] && arr[4] === arr[6])return console.log("Ganador",arr[2])
+    if(arr[0] === arr[4] && arr[4] === arr[8])return elementoWin(arr[0]);
+    if(arr[2] === arr[4] && arr[4] === arr[6])return elementoWin(arr[2]);
 }
 
 window.onload = start;
